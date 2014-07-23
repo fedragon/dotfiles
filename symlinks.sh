33b2f2905e796d4361e201ac32ebb2b47573d3ab
Federico Ragona <federico.ragona@gmail.com>
2014-07-23 10:39:16 +0200

    Generate symlinks

---
+++ symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm ~/.config/fish/config.fish
+ln -s ~/Workspace/dotfiles/config.fish ~/.config/fish/config.fish
+
+rm ~/.config/fish/config.fish
+ln -s ~/Workspace/dotfiles/gitconfig ~/.gitconfig
+
+rm ~/.config/fish/config.fish
+ln -s ~/Workspace/dotfiles/vimrc ~/.vimrc
+
+rm ~/.config/fish/config.fish
+ln -s ~/Workspace/oh-my-fish/ ~/.oh-my-fish
+
