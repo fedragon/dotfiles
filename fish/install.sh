bdacac30df72880903478d675544eef4c8933e9c
Federico Ragona <federico.ragona@gmail.com>
2014-11-19 13:51:14 +0100

    Show fortune+cowsay when opening a new fish shell

--- fish/install.sh
+++ fish/install.sh
@@ -10,3 +10,6 @@ git clone https://github.com/fedragon/oh-my-fish.git ~/Playground/oh-my-fish
 
 ~/Playground/oh-my-fish/tools/install.sh | fish
 
+## Custom functions
+rm ~/.oh-my-fish/functions/fish_greeting.fish
+cp ~/Playground/dotfiles/fish/fish_greeting.fish ~/.oh-my-fish/functions/fish_greeting.fish
