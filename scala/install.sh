2f0600aba176b92853485b68335f9ad15e8d96af
Federico Ragona <federico.ragona@gmail.com>
2014-12-23 20:51:14 +0100

    Install sbt via brew

--- scala/install.sh
+++ scala/install.sh
@@ -8,6 +8,8 @@ svm install scala-2.11.2
 
 set -U fish_user_paths $fish_user_paths ~/.svm/current/rt/bin
 
+brew install sbt
+
 ## SBT global plugins
 mkdir -p ~/.sbt/0.13/plugins
 ln -s ~/Playground/dotfiles/scala/plugins.sbt ~/.sbt/0.13/plugins/plugins.sbt
