db0ba97fe93b97c9c14040a108fe1e3b158ec89b
Federico Ragona <federico.ragona@gmail.com>
2015-03-21 23:28:35 +0100

    Remove oh-my-fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,4 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Theme
-# set fish_theme robbyrussell
-
-alias tmux="tmux -2"
-
-# set fish_key_bindings fish_vi_key_bindings
-# set fish_bind_mode insert
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
 # Load environment variables and components
-
 set -x EDITOR vim
 set -x MAVEN_OPTS "-Xmx512m"
 
@@ -29,8 +7,8 @@ set -x LANG   "en_US.UTF-8"
 
 switch (uname)
   case Darwin
-    # Powerline integration
 
+    # Powerline integration
     powerline-daemon -q
     set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
     powerline-setup
--- fish/install.sh
+++ fish/install.sh
@@ -4,10 +4,6 @@
 brew install fish
 chsh -s /usr/local/bin/fish
 
-## Oh my fish
-git clone https://github.com/fedragon/oh-my-fish.git ~/Playground/oh-my-fish
-~/Playground/oh-my-fish/tools/install.sh | fish
-
 ## Custom functions
 mkdir -p .config/fish/functions
 rm ~/.config/fish/functions/*
