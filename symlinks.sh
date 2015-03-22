6a9dbe787958ede32cbed451a77f1159cc3c7eba
Federico Ragona <federico.ragona@gmail.com>
2015-03-22 22:01:19 +0100

    Remove obsolete symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -7,11 +7,7 @@ ln -sf ~/Playground/dotfiles/git/gitignore ~/.gitignore
 
 ln -sf ~/Playground/dotfiles/vim/vimrc ~/.vimrc
 
-ln -sf ~/Playground/oh-my-fish/ ~/.oh-my-fish
-
 ln -sf ~/Playground/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 ln -sf ~/Playground/dotfiles/ctags ~/.ctags
-
-ln -sf ~/Playground/dotfiles/thymerc ~/.thymerc
 
