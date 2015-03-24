c3af1b81fe6839db02cc2457b25cdedea4cac456
Federico Ragona <federico.ragona@gmail.com>
2015-03-24 08:02:21 +0100

    Add pentadactyl configuration

---
+++ pentadactyl/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/claytron/pentadactyl-solarized.git .pentadactyl/
+
+ln -sf ~/Playground/dotfiles/pentadactyl/pentadactylrc ~/.pentadactylrc
+
+echo "Don't forget to install Pentadactyl in Firefox!"
