27b0ce80552d8d3bba17fdf2e7d93cd3e9414a16
Federico Ragona <federico.ragona@gmail.com>
2015-06-17 18:05:22 +0200

    fish,git: open emacs in new frame

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -4,5 +4,5 @@ function aliases
   alias ll "ls -lhaG"
   alias tm "tmux attach -t default; or tmux new -s default"
   alias tmux "tmux -2"
-  alias vim "emacsclient -nw" 
+  alias vim "emacsclient -t"
 end
