e04681b0e75cf1ba74b543e544e6edffc9d82e43
Federico Ragona <federico.ragona@gmail.com>
2015-06-21 15:01:40 +0200

    install Seil along with Karabiner
    to remap Caps Lock to Escape/Ctrl

--- karabiner/install.sh
+++ karabiner/install.sh
@@ -3,3 +3,7 @@
 brew cask install karabiner
 
 ln -sf $(pwd)/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+
+brew cask install seil
+
+echo "TODO: Set Caps Lock to Escape (keycode 80) in Seil!"
