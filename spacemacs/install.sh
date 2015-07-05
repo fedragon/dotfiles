aeacbaca0b46f78dd9814512cc3e520bd167cdf0
Federico Ragona <federico.ragona@gmail.com>
2015-07-05 15:21:17 +0200

    spacemacs: symlink private layers one by one
    to avoid git issues when updating spacemacs

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -7,4 +7,4 @@ brew install coreutils
 git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 ln -sf $(pwd)/spacemacs ~/.spacemacs
-ln -sf $(pwd)/spacemacs/private ~/.emacs.d/
+ln -sf $(pwd)/spacemacs/private/fedragon ~/.emacs.d/private/
