e1e61ababf16fa06fd5979cedae86323b924e922
Federico Ragona <federico.ragona@gmail.com>
2016-12-29 22:04:29 +0100

    tmux: blue colors

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,3 +5,6 @@ brew install tmux
 ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+brew install reattach-to-user-namespace
+brew install urlview
