14828bdde49f713900f6b21f26212947ee20eff7
Federico Ragona <federico.ragona@gmail.com>
2020-03-11 20:20:20 +0100

    Configure Fish plugins

--- fish/install.sh
+++ fish/install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Fish
 brew install fish
 chsh -s /usr/local/bin/fish
 
 mkdir -p ~/.config/fish
 
 ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
+ln -sf $(pwd)/fish/fishfile ~/.config/fish/fishfile
 
-## Custom functions
-mkdir -p ~/.config/fish/functions
-rm ~/.config/fish/functions/*
-
-cp $(pwd)/fish/functions/* ~/.config/fish/functions/
+rm -r ~/.config/fish/functions
+cp -r $(pwd)/fish/functions ~/.config/fish/
 
