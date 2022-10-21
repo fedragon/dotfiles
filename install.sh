8da2cc1d06869b407c30fba1c2e115b78684acbe
Federico Ragona <federico.ragona@gmail.com>
2022-10-21 11:32:23 +0200

    general: fix installation order

--- install.sh
+++ install.sh
@@ -3,21 +3,19 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew tap homebrew/cask-cask
 
+brew install go
+
 ./git/install.sh
 
 ./cuttlefish/install.sh
+
+./starship/install.sh
 
 ./fish/install.sh
 
 ./karabiner/install.sh
 
 ./vim/install.sh
-
-brew cask install homebrew/cask-versions/java8
-
-brew install bat exa
-
-./starship/install.sh
 
 brew cask install 1password amethyst docker dropbox firefox goland iterm2 obsidian slack
 
